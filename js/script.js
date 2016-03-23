7b793c8f25ec65f00d7a5dd52c4d807fb11abf72
Divya Karunakaran <divyakarunakaran1990@gmail.com>
2016-03-24 00:02:06 +1300

    Online Kids shopping site
    Responsive site developed using HTML5, CSS3, BootStrap3 and JavaScript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-document.getElementById("foot01").innerHTML= "<p> &copy; "+ new Date().getFullYear() +" ForKids.  All Right reserverd by Ramya</p>";
+document.getElementById("foot01").innerHTML= "<p> &copy; "+ new Date().getFullYear() +" ForKids.  All Right reserverd by Divya Karunakaran</p>";
